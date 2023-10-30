fda4f9c25cd529d632738f5b2262fb5400680a9b
Daniel <daniel.orti91@gmail.com>
2023-10-29 21:52:49 -0300

    Se generan archivos para la sección de alumnos

---
+++ instituto/Alumno/includes/footer_alumno.php
@@ -0,0 +1,30 @@
+    <!-- Essential javascripts for application to work-->
+    <script src="/instituto/js/jquery-3.3.1.min.js"></script>
+    <script src="/instituto/js/popper.min.js"></script>
+    <script src="/instituto/js/bootstrap.min.js"></script>
+    <script src="/instituto/js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="/instituto/js/plugins/pace.min.js"></script>
+    <script src="/instituto/js/fontawesome.js"></script>
+
+    <script type="text/javascript" src="/instituto/js/plugins/sweetalert.min.js"></script>
+    <script type="text/javascript" src="/instituto/vali-admin-master/docs/js/plugins/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="/instituto/vali-admin-master/docs/js/plugins/dataTables.bootstrap.min.js"></script>
+    <script type="text/javascript" src="/instituto/Adman/js/funtion-usuarios.js"></script>
+    <script type="text/javascript" src="/instituto/Adman/js/funtions_profesor.js"></script>
+    <script type="text/javascript" src="/instituto/Adman/js/funtion-alumno.js"></script>
+    <script type="text/javascript" src="/instituto/Adman/js/funtion-carrera.js"></script>
+    <script type="text/javascript" src="/instituto/Adman/js/funtion-materia.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.13.3/js/standalone/selectize.min.js"></script>
+    
+
+     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.2.0/js/tableexport.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.4/xlsx.full.min.js"></script>-->
+
+
+
+
+
+  </body>
+</html>
--- instituto/Alumno/index.php
+++ instituto/Alumno/index.php
@@ -1,6 +1,21 @@
 <?php
-require_once 'includes/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/instituto/Includes/load.php';
 ?>
+
+<?php
+
+if ($result["fk_Rol"]==1) {
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/instituto/Adman/includes/header_alumnos.php';
+} elseif ($$result["fk_Rol"]==2) {
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/instituto/Adman/includes/header.php';
+} else {
+   // Encabezado por defecto
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/instituto/Adman/includes/header.php';
+}
+?>
+
+
+
 <main class="app-content">
   <div class="row">
     <div class="col-md-12">
@@ -15,3 +30,21 @@ require_once 'includes/header.php';
 <?php
 require_once 'includes/footer.php';
 ?>
+
+
+<?php
+//require_once $_SERVER['DOCUMENT_ROOT'] . '/instituto/Includes/load.php';
+
+// Obtén el rol del usuario
+//$rol_usuario = obtenerRolUsuario();
+
+//if ($rol_usuario == 'rol1') {
+//    require_once $_SERVER['DOCUMENT_ROOT'] . '/instituto/Adman/includes/header_rol1.php';
+//} elseif ($rol_usuario == 'rol2') {
+//    require_once $_SERVER['DOCUMENT_ROOT'] . '/instituto/Adman/includes/header_rol2.php';
+//} else {
+    // Encabezado por defecto
+//    require_once $_SERVER['DOCUMENT_ROOT'] . '/instituto/Adman/includes/header.php';
+//}
+?>
+
